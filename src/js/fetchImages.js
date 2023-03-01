12d31a00c88e3bb5e19b41edc4d5604e1d071972
OlgaBaida <109526437+OlgaBaida@users.noreply.github.com>
2023-03-01 07:52:11 +0200

    fix bugs

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const axios = require('axios').default;
+// const axios = require('axios').default;
  export default async function fetchImages(value, page) {
     try {
         const URL = 'https://pixabay.com/api/';
